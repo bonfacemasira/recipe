326b8debac3eeb577ca6791531add7e49aab5b43
bonfacemasira <bonfacemasira@gmail.com>
2024-09-20 02:51:40 +0300

    create recipe module

---
+++ src/recipe/dto/create-recipe.dto.ts
@@ -0,0 +1,16 @@
+import { IsOptional, IsString } from 'class-validator';
+
+export class CreateRecipeDto {
+  @IsString()
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsString()
+  ingredients: string;
+
+  @IsString()
+  instructions: string;
+}
