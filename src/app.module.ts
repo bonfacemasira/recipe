368bbfb6d69545e81ffa8a77ee9c75479a496da9
bonfacemasira <bonfacemasira@gmail.com>
2024-10-10 04:30:28 +0300

    add posts, user and profile modules  to the parent

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { RecipeModule } from './recipe/recipe.module';
+import { PostsModule } from './posts/posts.module';
+import { ProfilesModule } from './profiles/profiles.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, RecipeModule],
+  imports: [
+    PrismaModule,
+    RecipeModule,
+    PostsModule,
+    ProfilesModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
